3b6677e77638eb7cafadac9b7767a75514ab8f49
KazooTTT <yjwang.echo@gmail.com>
2023-03-27 19:13:27 +0800

    chore:修改match url

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       userscript: {
         icon: 'https://vitejs.dev/logo.svg',
         namespace: 'npm/vite-plugin-monkey',
-        match: ['https://www.google.com/'],
+        match: ['https://2550505.com/'],
       },
     }),
   ],
