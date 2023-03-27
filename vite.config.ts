579471f017a77dfd9c3d779414a3545e7731de08
KazooTTT <yjwang.echo@gmail.com>
2023-03-27 21:06:15 +0800

    feat:add userscript config

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,12 @@ export default defineConfig({
     monkey({
       entry: "src/main.ts",
       userscript: {
-        icon: "https://vitejs.dev/logo.svg",
-        namespace: "npm/vite-plugin-monkey",
+        icon: "https://github.com/kazoottt.png",
+        namespace: "https://github.com/KazooTTT/mgclub-evolve",
         match: ["https://2550505.com/"],
+        author: "KazooTTT",
+        description: "展示毛怪俱乐部每个帖子最新的回复时间",
+        version: "0.0.1",
       },
     }),
   ],
